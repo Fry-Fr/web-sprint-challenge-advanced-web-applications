870bae10f9dd6121a92f018477c02877c359696c
Fry-Fr <ryjlwdg89@gmail.com>
2021-06-13 20:47:41 -0400

    BubblePage tests pass; MVP

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
+import fetchColorService from '../services/fetchColorService';
+jest.mock('../services/fetchColorService');
+fetchColorService.mockReturnValue([
+    {
+        color: "aliceblue",
+        code: {
+          hex: "#f0f8ff",
+        },
+        id: 1,
+      },
+      {
+        color: "limegreen",
+        code: {
+          hex: "#99ddbc",
+        },
+        id: 2,
+      }
+])
 
 test("Renders without errors", ()=> {
-    
+    render(<BubblePage />);
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    render(<BubblePage />);
+    expect(fetchColorService).toHaveBeenCalledTimes(2);
+    expect(fetchColorService()).toHaveLength(2);
 });
